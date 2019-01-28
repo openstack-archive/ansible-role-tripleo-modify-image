8b82f78e359b016a76eba6331b5b27cdf8fd6c68
Bogdan Dobrelya <bdobreli@redhat.com>
2019-01-28 12:53:14 +0100

    Track obsoleted packages replacements for updates
    Co-authored-by: Yatin Karel <ykarel@redhat.com>
    Change-Id: I5dfe91238898208aedfeb6623c62b97a9398cecb
    Closes-bug: #1813533
    Signed-off-by: Bogdan Dobrelya <bdobreli@redhat.com>

--- files/yum_update.sh
+++ files/yum_update.sh
@@ -5,7 +5,7 @@ set -eux
 packages_for_update=
 if [ -n "$1" ] && command -v repoquery >/dev/null 2>&1; then
     installed=$(rpm -qa --qf "%{NAME}\n" | sort)
-    available=$(repoquery --disablerepo='*' --enablerepo=$1 --qf %{NAME} -a | sort)
+    available=$(repoquery --provides --disablerepo='*' --enablerepo=$1 --qf %{NAME} -a | cut -d= -f1 | sort)
     packages_for_update=$(comm -12 <(printf "%s\n" $installed) <(printf "%s\n" $available))
 fi
 
